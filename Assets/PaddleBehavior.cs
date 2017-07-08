34288137c85fa7849d5d3e6dfa37f9db07877a40
adarshnush <h1410003@nushigh.edu.sg>
2017-07-08 23:57:10 +0800

    Created project
    Lessons 1&2

---
+++ Assets/PaddleBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehavior : MonoBehaviour {
+    public KeyCode keyLeft = KeyCode.LeftArrow;
+    public KeyCode keyRight = KeyCode.RightArrow;
+    public int paddleSpeed = 5;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        if (Input.GetKey (keyRight) && pos.x < 5.7)
+        {
+            pos.x += paddleSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(keyLeft) && pos.x < 5.7)
+        {
+            pos.x -= paddleSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
